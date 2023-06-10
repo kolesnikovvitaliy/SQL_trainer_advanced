a68d12f4381504038adbe76cb1212d54bb705928
Колесников Виталий <kolesnikovvitaliy@mail.ru>
2023-06-10 21:04:53 +0300

    отредактировал manager, ru_p_in_position

--- 1_Основы_SQL_расширенные_возможности/1_2_Простая_выборка_Резюме/1_2_3_Русская_буква_р_в_названиии/ru_p_in_position.sql
+++ 1_Основы_SQL_расширенные_возможности/1_2_Простая_выборка_Резюме/1_2_3_Русская_буква_р_в_названиии/ru_p_in_position.sql
@@ -1,11 +1,4 @@
-SELECT author as Автор, title as Книга, amount as Количество,
-    CASE 
-        WHEN amount >= 10 THEN 15
-        ELSE 0
-    END as Скидка,
-    CASE 
-        WHEN amount >= 10 THEN ROUND(price * 0.85, 2) 
-        ELSE price
-    END as Новая_цена
-FROM book
-ORDER BY Автор, Книга;
+SELECT DISTINCT applicant, position, date_birth
+FROM resume 
+WHERE position LIKE '%р'
+order by position, date_birth asc;
